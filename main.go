0801b7c09fb6b005ae286a1a605ea991b8c18b25
HotPaper77 <e.gbaguidi@tbs-education.org>
2024-04-01 15:42:57 +0200

    buffuring output to ad-hoc file and adding stock as cli flag

--- main.go
+++ main.go
@@ -1,29 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"sync"
 	"time"
 )
 
+const (
+	outputFilename = "count_output.txt"
+)
+
 var (
-	maxWorkers = runtime.NumCPU()
-	sourceDir  string
-	incoming   chan Task
-	done       chan Task
-	workersWg  sync.WaitGroup
-	wg         sync.WaitGroup
+	maxWorkers      = runtime.NumCPU()
+	stockColumnName string
+	sourceDir       string
+	incoming        chan Task
+	done            chan Task
+	workersWg       sync.WaitGroup
+	wg              sync.WaitGroup
 )
 
 type Task struct {
-	path   string
-	result int
+	path          string
+	totalProducts int
+	inStock       int
+}
+
+type parseError struct {
+	line int
+	err  error
+}
+
+func (e *parseError) Error() string {
+	errorMessage := fmt.Sprintf("could not parse record line %d. error:%v\n", e.line, e.err)
+	return errorMessage
+}
+
+type noStockColumn struct{}
+
+func (e *noStockColumn) Error() string {
+	errorMessage := fmt.Sprintln("column stock not present in file")
+	return errorMessage
+}
+
+func (t *Task) Write(w *bufio.Writer) {
+	message := fmt.Sprintf("%s,%d,%d\n", t.path, t.totalProducts, t.inStock)
+
+	_, err := w.WriteString(message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func worker(incoming chan Task, done chan Task) {
@@ -32,7 +68,7 @@ func worker(incoming chan Task, done chan Task) {
 		err := process(t.path, &t)
 
 		if err != nil {
-			fmt.Printf("Could not process %s. Error: %v", t.path, err)
+			fmt.Printf("Could not process file %s. Error: %v", t.path, err)
 		}
 
 		done <- t
@@ -56,6 +92,10 @@ func Visit(path string, d fs.DirEntry, err error) error {
 	return nil
 }
 
+func init() {
+	flag.StringVar(&stockColumnName, "stock", "stock", "enter the name of the column stock. default is 'stock'")
+}
+
 func main() {
 
 	start := time.Now()
@@ -76,13 +116,26 @@ func main() {
 
 	done = make(chan Task, 200)
 
+	file, err := os.Create(outputFilename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Writing to output file: %s\n", outputFilename)
+
+	writer := bufio.NewWriter(file)
+
+	defer writer.Flush()
+
 	wg.Add(1)
-	go func() {
+	go func(w *bufio.Writer) {
 		defer wg.Done()
 		for t := range done {
-			fmt.Printf("%s,%d\n", t.path, t.result)
+			t.Write(w)
+			//fmt.Printf("%s,%d\n", t.path, t.result)
 		}
-	}()
+	}(writer)
 
 	for w := 1; w <= maxWorkers; w++ {
 		workersWg.Add(1)
@@ -108,11 +161,13 @@ func main() {
 
 	executionTime := time.Since(start)
 
-	log.Printf("Parsing Completed!\n Execution Time:%9.2f min\n", executionTime.Seconds())
+	log.Printf("Parsing Completed!\n Execution Time:%9.2f sec\n", executionTime.Seconds())
 
 }
 
 func process(path string, t *Task) error {
+
+	inStockCount := 0
 
 	fd, err := os.Open(path)
 
@@ -126,9 +181,49 @@ func process(path string, t *Task) error {
 
 	if err != nil {
 		return err
+	} else if len(records) == 0 {
+		t.totalProducts = 0
+		t.inStock = 0
+		return nil
 	}
 
-	t.result = len(records)
+	header := records[0]
+	headerMap := mapHeader(header)
+
+	for i, record := range records[1:] {
+
+		if indexColumnStock, ok := headerMap[stockColumnName]; ok {
+
+			stockValue, err := strconv.ParseFloat(record[indexColumnStock], 32)
+
+			if err != nil {
+				newError := parseError{line: i, err: err}
+				return &newError
+			}
+
+			if stockValue > 0 {
+				inStockCount++
+			}
+
+		} else {
+			newError := noStockColumn{}
+			return &newError
+		}
+
+	}
+	t.totalProducts = len(records) - 1
+	t.inStock = inStockCount
 
 	return nil
+}
+
+func mapHeader(header []string) map[string]int {
+
+	headerMap := make(map[string]int)
+
+	for index, columName := range header {
+		headerMap[columName] = index
+	}
+
+	return headerMap
 }
